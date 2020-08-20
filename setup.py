0261d7e9de5703c4d6e0c1ba96ce9a420b68b2b6
UdhavPawar <upawar78@gmail.com>
2020-08-19 22:47:08 -0700

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(),
     version="0.1",
     description="Sync AWS secrets across multiple accounts",
-    long_description="This python package allows user to easily sync or replicate specifc or all secrets between multiple AWS accounts."
+    long_description="This python package allows user to easily sync or replicate specifc or all secrets between multiple AWS accounts.",
     author="Udhav Pawar",
     author_email="upawar78@gmail.com",
     url="https://github.com/UdhavPawar/aws-sync",
